0950f051e665010501c30bd397045fb6f0d7c22e
Nayuki Minase <nayuki@eigenstate.org>
2011-08-05 01:34:30 +0000

    FrequencyTable: Added method comments, added field 'total' and the logic for it, tweaked code.

--- src/nayuki/arithcode/FrequencyTable.java
+++ src/nayuki/arithcode/FrequencyTable.java
@@ -3,12 +3,16 @@ package nayuki.arithcode;
 
 /**
  * A table of symbol frequencies. Mutable.
+ * Each symbol has a frequency, which is a non-negative integer.
+ * The total of all symbol frequencies must not exceed Integer.MAX_VALUE.
  */
 public final class FrequencyTable {
 	
 	private int[] frequencies;
 	
 	private int[] cumulative;  // Initialized lazily
+	
+	private int total;
 	
 	
 	
@@ -19,20 +23,24 @@ public final class FrequencyTable {
 			throw new IllegalArgumentException("At least 2 symbols needed");
 		
 		frequencies = freqs.clone();  // Defensive copy
+		total = 0;
 		for (int x : frequencies) {
 			if (x < 0)
 				throw new IllegalArgumentException("Negative frequency");
+			total = checkedAdd(x, total);
 		}
 		cumulative = null;
 	}
 	
 	
 	
+	// Returns the number of symbols in this table.
 	public int getSymbolLimit() {
 		return frequencies.length;
 	}
 	
 	
+	// Returns the frequency of the symbol.
 	public int get(int symbol) {
 		if (symbol < 0 || symbol >= frequencies.length)
 			throw new IllegalArgumentException("Symbol out of range");
@@ -40,31 +48,35 @@ public final class FrequencyTable {
 	}
 	
 	
+	// Sets the frequency of the symbol.
 	public void set(int symbol, int freq) {
 		if (symbol < 0 || symbol >= frequencies.length)
 			throw new IllegalArgumentException("Symbol out of range");
+		total = checkedAdd(total - frequencies[symbol], freq);
 		frequencies[symbol] = freq;
 		cumulative = null;
 	}
 	
 	
+	// Increments the frequency of the symbol.
 	public void increment(int symbol) {
 		if (symbol < 0 || symbol >= frequencies.length)
 			throw new IllegalArgumentException("Symbol out of range");
 		if (frequencies[symbol] == Integer.MAX_VALUE)
 			throw new RuntimeException("Arithmetic overflow");
+		total = checkedAdd(1, total);
 		frequencies[symbol]++;
 		cumulative = null;
 	}
 	
 	
+	// Returns the total of all symbol frequencies.
 	public int getTotal() {
-		if (cumulative == null)
-			initCumulative();
-		return cumulative[frequencies.length];
+		return total;
 	}
 	
 	
+	// Returns the total of the frequencies of all the symbols below the given one.
 	public int getLow(int symbol) {
 		if (symbol < 0 || symbol >= frequencies.length)
 			throw new IllegalArgumentException("Symbol out of range");
@@ -74,6 +86,7 @@ public final class FrequencyTable {
 	}
 	
 	
+	// Returns the total of the frequencies of this symbol and all the ones below.
 	public int getHigh(int symbol) {
 		if (symbol < 0 || symbol >= frequencies.length)
 			throw new IllegalArgumentException("Symbol out of range");
@@ -88,11 +101,10 @@ public final class FrequencyTable {
 		int sum = 0;
 		for (int i = 0; i < frequencies.length; i++) {
 			cumulative[i] = sum;
-			long temp = sum + frequencies[i];
-			if (temp > Integer.MAX_VALUE)
-				throw new RuntimeException("Arithmetic overflow");
-			sum = (int)temp;
+			sum = checkedAdd(frequencies[i], sum);
 		}
+		if (sum != total)
+			throw new AssertionError();
 		cumulative[frequencies.length] = sum;
 	}
 	
@@ -103,6 +115,16 @@ public final class FrequencyTable {
 		for (int i = 0; i < frequencies.length; i++)
 			sb.append(String.format("%d\t%d%n", i, frequencies[i]));
 		return sb.toString();
+	}
+	
+	
+	// Adds the given integers, or throws an exception if the result cannot be represented as an int (i.e. overflow).
+	private static int checkedAdd(int x, int y) {
+		int z = x + y;
+		if (y > 0 && z < x || y < 0 && z > x)
+			throw new ArithmeticException("Arithmetic overflow");
+		else
+			return z;
 	}
 	
 }
