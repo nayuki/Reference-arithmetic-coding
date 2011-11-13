587d5c10a007b79da44884f8eb51331302b2893a
Nayuki Minase <nayuki@eigenstate.org>
2011-11-13 00:08:48 +0000

    FlatFrequencyTable: Added class.

---
+++ src/nayuki/arithcode/FlatFrequencyTable.java
@@ -0,0 +1,61 @@
+package nayuki.arithcode;
+
+
+/**
+ * A frequency table where every symbol has the same frequency of 1. Immutable.
+ */
+public final class FlatFrequencyTable implements FrequencyTable {
+	
+	private final int numSymbols;
+	
+	
+	
+	public FlatFrequencyTable(int numSyms) {
+		if (numSyms <= 0)
+			throw new IllegalArgumentException("Number of symbols must be positive");
+		numSymbols = numSyms;
+	}
+	
+	
+	
+	public int getSymbolLimit() {
+		return numSymbols;
+	}
+	
+	
+	public int get(int symbol) {
+		if (symbol < 0 || symbol >= numSymbols)
+			throw new IllegalArgumentException("Symbol out of range");
+		return 1;
+	}
+	
+	
+	public int getTotal() {
+		return numSymbols;
+	}
+	
+	
+	public int getLow(int symbol) {
+		if (symbol < 0 || symbol >= numSymbols)
+			throw new IllegalArgumentException("Symbol out of range");
+		return symbol;
+	}
+	
+	
+	public int getHigh(int symbol) {
+		if (symbol < 0 || symbol >= numSymbols)
+			throw new IllegalArgumentException("Symbol out of range");
+		return symbol + 1;
+	}
+	
+	
+	public void set(int symbol, int freq) {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	public void increment(int symbol) {
+		throw new UnsupportedOperationException();
+	}
+	
+}
