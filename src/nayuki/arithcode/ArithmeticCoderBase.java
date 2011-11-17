dbee32e3a352f5d06b173061138db9ade2d47860
Nayuki Minase <nayuki@eigenstate.org>
2011-11-17 17:59:54 +0000

    ArithmeticCoderBase: Tweaked code, added assertions, added comments.

--- src/nayuki/arithcode/ArithmeticCoderBase.java
+++ src/nayuki/arithcode/ArithmeticCoderBase.java
@@ -6,14 +6,15 @@ import java.io.IOException;
 public abstract class ArithmeticCoderBase {
 	
 	protected final long STATE_SIZE  = 32;
-	protected final long MASK        = (1L << STATE_SIZE - 0) - 1;
-	protected final long TOP_MASK    = (1L << STATE_SIZE - 1);
-	protected final long SECOND_MASK = (1L << STATE_SIZE - 2);
-	protected final long MAX_RANGE   = (1L << STATE_SIZE - 0);
-	protected final long MIN_RANGE   = (1L << STATE_SIZE - 2) + 2;
+	protected final long MASK        = (1L << (STATE_SIZE - 0)) - 1;  //  111...111
+	protected final long TOP_MASK    = (1L << (STATE_SIZE - 1));      //  100...000
+	protected final long SECOND_MASK = (1L << (STATE_SIZE - 2));      //  010...000
+	protected final long MAX_RANGE   = (1L << (STATE_SIZE - 0));      // 1000...000
+	protected final long MIN_RANGE   = (1L << (STATE_SIZE - 2)) + 2;  //   10...010
+	protected final long MAX_TOTAL   = Math.min(Long.MAX_VALUE / MAX_RANGE, MIN_RANGE);
 	
-	protected long low;   // Has an infinite number of trailing zeros
-	protected long high;  // Has an infinite number of trailing ones
+	protected long low;   // Has an infinite number of trailing 0s
+	protected long high;  // Has an infinite number of trailing 1s
 	
 	
 	
@@ -26,18 +27,22 @@ public abstract class ArithmeticCoderBase {
 	
 	// Update the range as a result of seeing the symbol, i.e. update low and high.
 	protected void update(FrequencyTable freq, int symbol) throws IOException {
+		// State check
 		long range = high - low + 1;
-		long total = freq.getTotal();
-		long symLow = freq.getLow(symbol);
-		long symHigh = freq.getHigh(symbol);
-		
-		if (symLow == symHigh)
-			throw new IllegalArgumentException("Symbol has zero frequency");
 		if (low >= high || (low & MASK) != low || (high & MASK) != high)
 			throw new AssertionError("Low or high out of range");
 		if (range < MIN_RANGE || range > MAX_RANGE)
 			throw new AssertionError("Range out of range");
-		if (total > MIN_RANGE || Long.MAX_VALUE / total < MAX_RANGE)
+		
+		// Frequency table usage check
+		long total = freq.getTotal();
+		long symLow = freq.getLow(symbol);
+		long symHigh = freq.getHigh(symbol);
+		if (!(total > 0 && 0 <= symLow && symLow <= symHigh && symHigh <= total))
+			throw new IllegalArgumentException("Illegal values from frequency table");
+		if (symLow == symHigh)
+			throw new IllegalArgumentException("Symbol has zero frequency");
+		if (total > MAX_TOTAL)
 			throw new IllegalArgumentException("Cannot code symbol in range");
 		
 		// Update range
